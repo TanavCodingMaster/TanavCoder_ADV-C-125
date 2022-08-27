dc4a0c94fac783320d8e1da94dc77872c84262ce
TanavCodingMaster <95131595+TanavCodingMaster@users.noreply.github.com>
2022-08-27 18:46:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+noseX = 0;
+noseY = 0;
+difference= 0;
+rightWristX = 0;
+leftWristX = 0;
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500)
+    canvas = createCanvas(550, 550)
+    canvas.position(560,100)
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function draw() {
+    background('#969A97')  ;
+    fill('#F90093')
+    stoke('#F90093')
+    square(noseX, noseY, difference);
+
+    document.getElementById("square_side").innerHTML = "Width and Height of the sqare will be" + difference + "px";
+}
+function modelLoaded() {
+    console.log('PoseNet is initialized')
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " +noseX+"noseY = " +noseY );
+
+        leftWristX = results[0].pose.leftWrist.x
+        rightWristX = results[0].pose.rightWrist.x
+        difference = floor(leftWristX - rightWristX)
+
+        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference)
+    }
+}
